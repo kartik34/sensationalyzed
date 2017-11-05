e5a30b442b581cced4bd4e5885ac5088739b07d4
Kartik Singhal <kartiksinghal3.14@gmail.com>
2017-11-05 08:46:48 +0000

    updates

--- app.js
+++ app.js
@@ -285,6 +285,10 @@ app.get("/:site", function(req,res){
 })
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("server is running");
-});
+
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
